368359c9c3493c004ef5015eae05d339f1432105
xmwang9516 <xmwang9516@users.noreply.github.com>
2014-09-20 22:18:05 +0800

    Update MySQLDataSource.java

--- MySQLDataSource.java
+++ MySQLDataSource.java
@@ -11,7 +11,7 @@ public class MySQLDataSource {
 	private static MysqlDataSource dataSource;
 	public static synchronized MysqlDataSource get(){
 		if(dataSource == null){
-			String jdbcurl = "jdbc:mysql://172.18.109.56:3306/sense_deeplearning?useUnicode=true&amp;characterEncoding=UTF-8&amp;charSet=UTF-8";
+			String jdbcurl = "jdbc:mysql://172.xx.xx.xx:3306/sense_deeplearning?useUnicode=true&amp;characterEncoding=UTF-8&amp;charSet=UTF-8";
 			String userName = "sense";
 			String password = "sense123";
 			dataSource = new MysqlDataSource(jdbcurl, userName, password);
